7d1b21a7b0c0a24316bc4e3361d465c5ad618d20
amos odeke <amodekejp@gmail.com>
2022-10-19 10:08:47 +0100

    Added example one file

---
+++ example1.py
@@ -0,0 +1,8 @@
+name = input("what is your name ")
+age = input("what is your age ")
+height = input("what is your height ")
+if age == ">30":
+    print("you are not allowed to run this program")
+else :
+    print(" welcome to this class")
+
